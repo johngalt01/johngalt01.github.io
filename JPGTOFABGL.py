237f172e6667ad9d560f39c8c1da27c25c998af1
johngalt01 <117768224+johngalt01@users.noreply.github.com>
2023-12-08 21:35:44 -0500

    update 0.63.2
    added 2 modes black and white and color mode for picture display

--- JPGTOFABGL.py
+++ JPGTOFABGL.py
@@ -1,6 +1,11 @@
-# Ver 0.63.1 12/5/2023 
+# Ver 0.63.2 12/8/2023 
 # By John Galt Furball1985
-# Clean Up
+
+# added new arguement 7 which has 2 B&W picture modes and the Color mode
+
+# you can also compile this script to make it faster
+# python -m py_compile jpgtofabgl creates jpgtofabglc
+# make sure to change permissions to allow it to run : chmod +x jpgtofabglc
 
 from PIL import Image
 import sys, termios, tty, os, time
@@ -17,7 +22,8 @@ def getch():
     return ch
 
 
-# python JPGTOFABGL % A T 0 0 100
+# python JPGTOFABGL % A T 0 0 100 2: set Auto mode, Transparent pictures, x(not used), y(not used), width 100 pixels, color mode
+# jpgtofabglc % M N 25 75 50 0: set Manual mode, solid picture, x=25, y=75, width 50 pixels, 1 bit b&w mode
 
 arg1 = sys.argv[1] # filename
 arg2 = sys.argv[2] # Auto, Semi, Manual mode
@@ -25,22 +31,34 @@ arg3 = sys.argv[3] # T transparency 'T' or not 'N'ot a 't'
 arg4 = sys.argv[4] # X
 arg5 = sys.argv[5] # Y
 arg6 = sys.argv[6] # resize limit 
+arg7 = sys.argv[7] # 0: black and white 1 bit with dither; 1: black and white greyscale; 2: color mode
+
 
 esc=chr(27)
 
-FILENAME=arg1 # Image can be in Gif Jpeg or Png format
+FILENAME=arg1 #image can be in gif jpeg or png format
 
-# Automatic Resize and Dithering
+#automatic resize
 
 image = Image.open(FILENAME)
 tempimage='temp.png'
 
 image.thumbnail((int(arg6),int(arg6)))
 
-imageout = image.convert("P",dither=Image.FLOYDSTEINBERG, palette=Image.WEB, colors=64)
-imageout.save(tempimage)
+if arg7 == "0":
+ imageout = image.convert("1",dither=Image.FLOYDSTEINBERG, palette=Image.WEB, colors=1)
+ imageout.save(tempimage)
+ im=Image.open(tempimage).convert('RGB')
 
-im=Image.open(tempimage).convert('RGB')
+elif arg7 == "1":
+ imageout = image.convert("L",dither=Image.FLOYDSTEINBERG, palette=Image.WEB, colors=4)
+ imageout.save(tempimage)
+ im=Image.open(tempimage).convert('RGB') 
+
+else:
+ imageout = image.convert("P",dither=Image.FLOYDSTEINBERG, palette=Image.WEB, colors=64)
+ imageout.save(tempimage)
+ im=Image.open(tempimage).convert('RGB')
 
 pix=im.load()
 w=im.size[0]
@@ -85,7 +103,7 @@ if arg2=="S" or arg2=="s":
  elif key=="Q" or key=="q" or key==chr(27):
   exit(0)
 
- else: # Default to a center image if keypress is wrong
+ else:
  # exit(0)
   PS=512-YY
   PS2=384-XX
